fb7efeff7fab2003a9b6d5c8197d74a35cd787da
RamonLzSouza <ramonsouza0309@gmail.com>
2019-08-15 22:06:51 -0300

    Criação do componente de Arrow Function, Componentes Filhos, Componentes Classe

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,6 +1,21 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// //import {Primeiro,Segundo,Terceiro} from './component.jsx'
+// //import FamiliaSilva from './familiaSilva'
+// import Familia from './familia'
+// import Membro from './member'
+
+// ReactDOM.render(
+//         <Familia sobrenome='Silva '>
+//             <Membro nome='Guilherme ' />
+//             <Membro nome='Pedro ' />
+//             <Membro nome='Luis ' />
+//         </Familia>
+//     , document.getElementById('app'))
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Exemplo from './component.jsx'
-
-ReactDOM.render(<Exemplo/>,
-    document.getElementById('app'))
+import ClassComponent from './classComponent'
+ReactDOM.render(
+    <ClassComponent idade='21' />
+    , document.getElementById('app'))
---
+++ ex/member.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default props => (
+  <div>
+    Nome: {props.nome}
+    <strong>
+      Sobrenome: {props.sobrenome}
+    </strong>
+  </div>
+)
